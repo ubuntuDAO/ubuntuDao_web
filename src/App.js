ad7f94b7c49bcc4c2b4921e9fda3123322c5fded
Fortunee5 <46243323+Fortunee5@users.noreply.github.com>
2022-01-04 05:24:19 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React from "react";
+import './new.css';
+//import './src/images';
+import Frame from "./Frame.svg";
+import Group from "./Group.svg";
+import Group5 from "./Group5.svg";
+import Group6 from "./Group6.svg";
+import Group7 from "./Group7.svg";
+import Frame19 from "./Frame19.svg";
+import Group10 from "./Group10.svg";
+import Group11 from "./Group11.svg";
+import Group12 from "./Group12.svg";
+export const App  = () => {
+  return (
+    <div className="desktop---8">
+      
+      <div className="frame-1">
+        <img src={Frame} className="App-Frmae" alt="Frame" />
+        <div className="frame-1-5">
+          <div className="frame-1-4">
+            <img src="" />
+          </div>
+        </div>
+        <p className="text-9">UBUNTU DAO</p>
+        <p className="text-1-0">Promoting Blockchain Adoption and a creator eco system in Africa</p>
+        <div className="frame-9">
+          <img src="" />
+          <p className="text-1-2">WHITE PAPER</p>
+        </div>
+        <div className="frame-1-7">
+          <img src="" />
+          <div className="ellipse-1" />
+          <div className="frame-1-0" />
+          <div className="frame-1-1" />
+          <div className="frame-1-2" />
+        </div>
+      </div>
+       <div className="group-9">
+        
+        <div className="frame-1-8">
+          <div className="frame-4">
+            <img src={Group} className="App-Group" alt="Group" />
+            <div className="ellipse-3-4" />
+          </div>
+          <div className="frame-5">
+            <img src=""/>
+            <div className="ellipse-3-4" />
+          </div>
+          
+          <div className="component-6">
+            <div className="component-6">
+              <div className="rectangle-7" />
+               <img src={Group11} className="App-Group11" alt="Group11" />
+            </div>
+          </div>
+          
+          
+        </div>
+        
+      </div>
+      <div className="frame-6">
+        <img src="" />
+        <div className="ellipse-3-4" />
+      </div>
+     
+      <div className="frame-6">
+        <img src="" />
+        <div className="ellipse-3-4" />
+      </div>
+      <div className="frame-7">
+         <div className="ellipse-3-4" />
+      </div>
+      <div className="frame-8">
+        <div className="frame-8">
+   
+        </div>
+      </div>
+      <div className="component-1">
+        <img src={Group12} className="App-Group12" alt="Group12" />
+      </div>
+ 
+      <img src="" />
+      <img src="" />
+      <div className="rectangle-4" />
+      <img src="" />
+      <div className="frame-2">
+        <div className="ellipse-2" />
+        <div className="ellipse-3" />
+        <div className="ellipse-4" />
+        <div className="ellipse-5" />
+        <div className="ellipse-6" />
+        <div className="ellipse-8" />
+        <div className="ellipse-7" />
+      </div>
+     
+    
+      <div className="group-3">
+      <img src={Group10} className="App-Group10" alt="Group10"/>
+    
+      </div>
+   
+   
+      <div className="group-5">
+        <img src="" />
+        <p className="text-7-5">Positioning</p>
+        <p className="text-7-6">To position Africa as a major player in the global tech landscape through the creation and adoption of innovative homegrown solutions utilizing Blockchain Technology.  </p>
+        <div className="ellipse-3-5" />
+        <img src="" />
+      </div>
+      <div className="group-6">
+        <img src="" />
+        <p className="text-7-8">Empowering</p>
+        <p className="text-7-9">To power the startup and scale of solution-centric Blockchain Projects in Africa. </p>
+        <div className="ellipse-3-5" />
+      </div>
+      <div className="group-7">
+        <img src="" />
+        <p className="text-8-0">Creativity</p>
+        <p className="text-8-1">To drive the creation and adoption of African products within Africa. </p>
+        <div className="ellipse-3-5" />
+      </div>
+      <div className="group-8">
+        <img src="" />
+        <p className="text-8-2">Promotion</p>
+        <p className="text-8-3">To promote the Pan-Africanist worldview and ignite unity amongst Africans.  </p>
+        <div className="ellipse-3-5" />
+      </div>
+      <img src="" />
+      <img src="" />
+       
+    </div>
+  )
+}
+export default App;
---
+++ src/pjec.js
@@ -0,0 +1,276 @@
+import React from "react";
+export const Desktop  = () => {
+  return (
+    <div className="desktop---8">
+      <div className="akar--icons-:telegram--fill">
+        <img src="" />
+      </div>
+      <img src="" />
+      <div className="akar--icons-:twitter--fill">
+        <img src="" />
+      </div>
+      <div className="akar--icons-:facebook--fill">
+        <img src="" />
+      </div>
+      <img src="" />
+      <div className="akar--icons-:reddit--fill">
+        <img src="" />
+      </div>
+      <div className="frame-1">
+        <img src="" />
+        <div className="frame-1-5">
+          <div className="frame-1-4">
+            <img src="" />
+          </div>
+        </div>
+        <p className="text-9">UBUNTU DAO</p>
+        <p className="text-1-0">Promoting Blockchain Adoption and a creator eco system in Africa</p>
+        <div className="frame-9">
+          <img src="" />
+          <p className="text-1-2">WHITE PAPER</p>
+        </div>
+        <div className="frame-1-7">
+          <img src="" />
+          <div className="ellipse-1" />
+          <div className="frame-1-0" />
+          <div className="frame-1-1" />
+          <div className="frame-1-2" />
+        </div>
+      </div>
+      <p className="text-1-3">OUR OBJECTIVES</p>
+      <div className="group-9">
+        <p className="text-1-4">OVERVIEW</p>
+        <p className="text-1-5">WHAT IS UBUNTU DAO?</p>
+        <p className="text-1-6">Collaborative community to provide Support</p>
+        <p className="text-1-7">It is a Blockchain Project that leverages the simplicity and communal spirit associated with meme tokens to drive Blockchain adoption and very importantly, promote a creator-ecosystem (web3 Startups) in Africa. The movement has already begun; to transit Africa from a (sadly) default consumption tilt to becoming production conscious and the Ubuntu Dao Project further emphasizes the truth that Africans are innovative enough and we can change ‘our’ world and sure enough ‘the’ world.              </p>
+        <div className="frame-1-8">
+          <div className="frame-4">
+            <img src="" />
+            <div className="ellipse-3-4" />
+          </div>
+          <div className="frame-5">
+            <img src="" />
+            <div className="ellipse-3-4" />
+          </div>
+          <div className="component-7">
+            <div className="component-7">
+              <div className="rectangle-6" />
+              <p className="text-2-0">100% COMMUITY DRIVEN</p>
+              <img src="" />
+            </div>
+          </div>
+          <div className="component-6">
+            <div className="component-6">
+              <div className="rectangle-7" />
+              <p className="text-2-2">PAN-AFRICAN APPROACH</p>
+              <img src="" />
+            </div>
+          </div>
+          <div className="component-5">
+            <div className="component-5">
+              <div className="rectangle-8" />
+              <p className="text-2-4">STRATEGIC CHARITY THROUGH DAOs</p>
+              <img src="" />
+            </div>
+          </div>
+          <div className="component-4">
+            <div className="component-4">
+              <div className="rectangle-9" />
+              <p className="text-2-6">TRANSPARENCY</p>
+              <img src="" />
+            </div>
+          </div>
+        </div>
+        <div className="component-8">
+          <div className="component-8">
+            <div className="rectangle-5" />
+            <p className="text-2-8">Read More</p>
+          </div>
+        </div>
+      </div>
+      <div className="frame-6">
+        <img src="" />
+        <div className="ellipse-3-4" />
+      </div>
+      <div className="frame-2-0">
+        <img src="" />
+        <div className="ellipse-3-4" />
+      </div>
+      <div className="frame-2-1">
+        <img src="" />
+        <div className="ellipse-3-4" />
+      </div>
+      <div className="frame-1-9">
+        <p className="text-3-2">TOKENOMICS</p>
+        <p className="text-3-3">TOKENOMICS</p>
+        <p className="text-3-4">PRESALE
+16.6% (25 trn) of the total circulating supply will be allocated for Presale. Presale buyers would have the privilege of significantly larger proportions for the airdrop.  
+
+AIR DROP
+In expressing our support for the Wakanda Inu Project, Ubuntu Dao tokens will be airdropped to all Wakanda Inu holders across supported wallets.  
+The Ubuntu Dao Airdrop will be deposited in three installments. As earlier stated, the first snapshot will be taken on Wakanda holders with 50% allocated Ubuntu tokens airdropped during the TGE. The other 50% will be airdropped at a 1:1 ratio on Ubuntu token holders in two installments.  </p>
+      </div>
+      <div className="frame-6">
+        <img src="" />
+        <div className="ellipse-3-4" />
+      </div>
+      <div className="frame-7">
+        <img src="" />
+        <div className="ellipse-3-4" />
+      </div>
+      <div className="frame-8">
+        <div className="frame-8">
+          <img src="" />
+          <p className="text-3-7">MAX SUPPLY</p>
+          <p className="text-3-8">500,000,000,000,000</p>
+        </div>
+      </div>
+      <div className="component-1">
+        <img src="" />
+      </div>
+      <div className="component-2">
+        <div className="component-2">
+          <img src="" />
+          <p className="text-4-0">0.000000025 </p>
+          <p className="text-4-1">PRESALE PRICE</p>
+        </div>
+      </div>
+      <div className="component-3">
+        <div className="component-3">
+          <img src="" />
+          <p className="text-4-2">150,000,000,000,000</p>
+          <p className="text-4-3">TOTAL SUPPLY</p>
+        </div>
+      </div>
+      <img src="" />
+      <img src="" />
+      <div className="rectangle-4" />
+      <img src="" />
+      <div className="frame-2">
+        <div className="ellipse-2" />
+        <div className="ellipse-3" />
+        <div className="ellipse-4" />
+        <div className="ellipse-5" />
+        <div className="ellipse-6" />
+        <div className="ellipse-8" />
+        <div className="ellipse-7" />
+      </div>
+      <div className="frame-3">
+        <div className="ellipse-9" />
+        <div className="ellipse-1-0" />
+        <div className="ellipse-1-1" />
+        <div className="ellipse-1-2" />
+        <div className="ellipse-1-3" />
+        <div className="ellipse-1-4" />
+        <div className="ellipse-1-5" />
+        <p className="text-4-7">20% LIQUIDITY</p>
+        <p className="text-4-8">20% ECOSYSTEM DEVELOPMENT</p>
+        <p className="text-4-9">16.67% PRESALE</p>
+        <p className="text-5-0">15% AIRDROP</p>
+        <p className="text-5-1">10% MARKETING </p>
+        <p className="text-5-2">10% UBUNTU FOUNDATION</p>
+        <p className="text-5-3">8.33% CHARITY</p>
+      </div>
+      <p className="text-5-4">TOKEN DISTRIBUTION</p>
+      <p className="text-5-5">UBUNTU DAO ROADMAP</p>
+      <img src="" />
+      <div className="group-3">
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <div className="ellipse-1-6" />
+        <div className="ellipse-1-7" />
+        <div className="ellipse-1-8" />
+        <div className="ellipse-1-9" />
+        <div className="ellipse-2-0" />
+        <div className="ellipse-2-1" />
+        <div className="ellipse-2-2" />
+        <div className="ellipse-2-3" />
+        <div className="ellipse-2-4" />
+      </div>
+      <div className="group-4">
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <div className="ellipse-1-6" />
+        <div className="ellipse-1-7" />
+        <div className="ellipse-1-8" />
+        <div className="ellipse-1-9" />
+        <div className="ellipse-2-0" />
+        <div className="ellipse-2-1" />
+        <div className="ellipse-2-2" />
+        <div className="ellipse-2-3" />
+      </div>
+      <p className="text-5-6">community building</p>
+      <p className="text-5-7">Centralized Exchange Listings</p>
+      <p className="text-5-8">website launch</p>
+      <p className="text-5-9">Auditing</p>
+      <p className="text-6-0">patnerships & support </p>
+      <p className="text-6-1">Release of the Ubuntu NFT Idea</p>
+      <p className="text-6-2">presale</p>
+      <p className="text-6-3">Development of the Blockchain Education Platform (Eduverse)</p>
+      <p className="text-6-4">airdrop support from  wakanda inu exchanges</p>
+      <p className="text-6-5">Concept Launch for the ‘Made in Africa Project’  </p>
+      <p className="text-6-6">snapshot of wakanda inu wallets on supported exchanges</p>
+      <p className="text-6-7"> Ubuntu NFTs Drop </p>
+      <p className="text-6-8">official TGE (First airdrop)</p>
+      <p className="text-6-9">Team Formulation and Development of E-commerce Platform </p>
+      <p className="text-7-0">decentralized exchange listing</p>
+      <p className="text-7-1">Roadmap Update </p>
+      <p className="text-7-2">listing in coinmarketcap & coingecko</p>
+      <div className="ellipse-3-3" />
+      <p className="text-7-3">White paper
+token
+roadmap
+contact us</p>
+      <p className="text-7-4">copyright@ubuntu dao</p>
+      <div className="group-5">
+        <img src="" />
+        <p className="text-7-5">Positioning</p>
+        <p className="text-7-6">To position Africa as a major player in the global tech landscape through the creation and adoption of innovative homegrown solutions utilizing Blockchain Technology.  </p>
+        <div className="ellipse-3-5" />
+        <img src="" />
+      </div>
+      <div className="group-6">
+        <img src="" />
+        <p className="text-7-8">Empowering</p>
+        <p className="text-7-9">To power the startup and scale of solution-centric Blockchain Projects in Africa. </p>
+        <div className="ellipse-3-5" />
+      </div>
+      <div className="group-7">
+        <img src="" />
+        <p className="text-8-0">Creativity</p>
+        <p className="text-8-1">To drive the creation and adoption of African products within Africa. </p>
+        <div className="ellipse-3-5" />
+      </div>
+      <div className="group-8">
+        <img src="" />
+        <p className="text-8-2">Promotion</p>
+        <p className="text-8-3">To promote the Pan-Africanist worldview and ignite unity amongst Africans.  </p>
+        <div className="ellipse-3-5" />
+      </div>
+      <img src="" />
+      <img src="" />
+      <div className="flat--color--icons-:advertising">
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+      </div>
+      <p className="text-8-8">OBJECTIVES</p>
+      <p className="text-8-9">+2347013395472
+info@Ubuntudao.com</p>
+    </div>
+  )
+}
